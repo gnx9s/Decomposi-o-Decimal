d1e67c3f6dae75f7fcaa4816b75029ee2711c0e6
gnx9s <gabriel.rodriguesm0rg@outlook.com>
2023-04-01 05:14:39 -0300

    exercicio resolvido com sucesso naquele pique nao muda nada

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Escreva um número inteiro: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+int resto = numero;
+
+int unidades = resto % 10;
+resto /= 10;
+
+int dezenas = resto % 10;
+resto /= 10;
+
+int unidade_de_milhar = resto;
+
+int centenas = unidade_de_milhar % 10;
+unidade_de_milhar /= 10;
+
+Console.WriteLine($"{unidades,10} unidade(s):");
+Console.WriteLine($"{dezenas,10} dezena(s):");
+Console.WriteLine($"{centenas,10} centena(s)");
+Console.WriteLine($"{unidade_de_milhar,10} unidade de milhar(es)");
